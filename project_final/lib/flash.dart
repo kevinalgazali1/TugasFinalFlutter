f3b9508acc501a232e779e2e65c5290929a9ff94
kevinalgazali1 <113749025+kevinalgazali1@users.noreply.github.com>
2024-06-13 23:09:18 +0800

    first commit

---
+++ project_final/lib/flash.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('images/logo.jpg', width: 150, height: 150), // pastikan jalur benar
+            const SizedBox(height: 200),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(300, 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                backgroundColor: const Color.fromARGB(255, 255, 215, 177),
+              ),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, '/home');
+              },
+              child: const Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
